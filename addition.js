366baba82d1a61f1deb457f160ff6214cd125660
Bhatt <shubham.a.bhatt@capgemini.com>
2018-12-03 18:23:21 +0530

    adds JavaScript day1 assignment

---
+++ addition.js
@@ -0,0 +1,8 @@
+add = function()
+{
+  var a =10;
+  var b =20;
+  var sum = a + b;
+  console.log("addition of "+a+" and "+b + " is "+ sum);
+}
+add();
---
+++ highestOf3.js
@@ -0,0 +1,10 @@
+highestOf3 = function()
+{
+    var x=12;
+    var y=1;
+    var z=28;
+
+var num = Math.max(x,y,z);
+console.log("heighest number is: "+num);
+}
+highestOf3();
